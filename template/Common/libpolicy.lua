ff072a7ae9a6481acb6d38cb7fae1d3d455b4546
mengxiang0811 <mengxiang0811@gmail.com>
2016-06-20 15:05:45 -0400

    save LPM rules to file

---
+++ template/Common/libpolicy.lua
@@ -0,0 +1,68 @@
+--[[
+This module will take a policy configuration table as input, and then extract
+the effective urls for further processing
+--]]
+
+local LIBPOLICY_MODULE = {}
+
+function LIBPOLICY_MODULE.print_policy_config(policy_config)
+
+    if policy_config == nil then
+        print("Error: an empty policy configuration!")
+        return
+    end
+
+    print("************The Policy Configuration************")
+    print("Policy name: " .. policy_config["name"])
+    print("Policy reference: " .. policy_config["ref"])
+
+    local url_tbl = policy_config["urls"]
+
+    for idx, url in pairs(url_tbl) do
+        print("\t############URL " .. idx .. "############")
+        print("\tURL: " .. url["link"])
+        print("\tUpdate interval: " .. url["interval"] .. " hour(s)/update")
+        print("\tPolicy group: " .. url["group"])
+        print("\tPolicy proto: " .. url["proto"])
+        print("\tPolicy parser: " .. url["parser"])
+        print("\tPolicy state: " .. url["state"])
+        print("\tPolicy description:") 
+        print("\t\t" .. url["desc"])
+        print("\t---------------------------")
+    end
+
+end
+
+function LIBPOLICY_MODULE.get_effective_policy(policy_config)
+    local effective_policy = {}
+
+    if policy_config == nil then
+        print("Error: an empty policy configuration!")
+        return effective_policy
+    end
+
+    effective_policy["name"] = policy_config["name"]
+    effective_policy["ref"] = policy_config["ref"]
+
+    local url_tbl = policy_config["urls"]
+
+    local eut = {}
+
+    for idx, url in pairs(url_tbl) do
+        if url["state"] == "used" then
+            eut[#eut + 1] = url_tbl[idx]
+        end
+    end
+
+    effective_policy["urls"] = eut
+
+    return effective_policy
+
+end
+
+--the loop to update a policy
+function LIBPOLICY_MODULE.policy_schedule(policy)
+
+end
+
+return LIBPOLICY_MODULE
